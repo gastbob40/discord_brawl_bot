3e15beb731e90039e344020d3dff47ea6f5905f0
Quentin Briolant <quentin.briolant@epita.fr>
2020-02-17 12:50:57 +0000

    feat (permissions): add function to add permissions

--- src/utils/permissions_manager.py
+++ src/utils/permissions_manager.py
@@ -16,3 +16,18 @@ class PermissionsManager:
                 return True
 
         return False
+
+    @staticmethod
+    def add_perm(perm_name: str, role_id: int) -> bool:
+        with open('src/_data/permissions.yml', 'r') as file:
+            permissions = yaml.safe_load(file)
+
+        if perm_name not in permissions:
+            raise Exception(f'{perm_name} not found in configuration file')
+
+        permissions[perm_name].append(role_id)
+
+        with open('src/_data/permissions.yml', 'w', encoding='utf8') as outfile:
+            yaml.dump(permissions, outfile, default_flow_style=False, allow_unicode=True)
+
+        return True
