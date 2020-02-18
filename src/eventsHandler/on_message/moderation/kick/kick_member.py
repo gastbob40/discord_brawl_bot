ea10cb5d9735aea83552ca6cdb15845e8aabb7ee
Quentin Briolant <quentin.briolant@epita.fr>
2020-02-18 22:17:36 +0000

    fix (kick): update code

--- src/eventsHandler/on_message/moderation/kick/kick_member.py
+++ src/eventsHandler/on_message/moderation/kick/kick_member.py
@@ -4,25 +4,46 @@ import discord
 import yaml
 
 from src.models.models import session, Kick
+from src.utils.embeds_manager import EmbedsManager
+from src.utils.permissions_manager import PermissionsManager
 
 
 async def kick_member(client: discord.Client, message: discord.Message, args: List[str]):
-    if not message.author.guild_permissions.kick_members:
-        await message.channel.send(f":x: Vous n'avez pas les permissions pour cette commande.")
-        return
+    with open('run/config/config.yml', 'r') as file:
+        config = yaml.safe_load(file)
 
-    # Check parameters
-    # ?kick @pseudo raison
-    # ?kick @pseudo -r raison
+    # Check permissions
+    if not PermissionsManager.has_perm(message.author, 'kick'):
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                "Vous n'avez pas les permissions pour cette commande."
+            )
+        )
+
+    # Help message
+    if args and args[0] == '-h':
+        return await message.channel.send(
+            embed=EmbedsManager.information_embed(
+                "Rappel de la commande : \n"
+                f"`{config['prefix']}kick <@pseudo> <reason>`\n"
+                f"`{config['prefix']}kick <@pseudo> -r <reason_id>`"
+            )
+        )
+
+    # Check inputs
     if len(message.mentions) != 1:
-        await message.channel.send(f":x: Erreur dans la commande. Merci de mentionner un utilisateur.")
-        return
-
-    args = args[1:]
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur dans la commande. Merci de mentionner un utilisateur."
+            )
+        )
 
     if len(args) < 2:
-        await message.channel.send(f":x: Erreur dans la commande. Merci de mettre une raison.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur dans la commande. Merci de mettre une raison."
+            )
+        )
 
     with open('run/config/reasons.yml', 'r', encoding='utf8') as file:
         reasons = yaml.safe_load(file)
@@ -35,8 +56,11 @@ async def kick_member(client: discord.Client, message: discord.Message, args: Li
             for reason_index in args[1:]:
                 current_reason += f"- {reasons[int(reason_index)]}\n"
         except:
-            await message.channel.send(f":x: Erreur dans la commande. Merci de mettre un index d'erreur valide.")
-            return
+            return await message.channel.send(
+                embed=EmbedsManager.error_embed(
+                    ":x: Erreur dans la commande. Merci de mettre un index d'erreur valide."
+                )
+            )
 
     else:
         # Custom reason
@@ -47,6 +71,9 @@ async def kick_member(client: discord.Client, message: discord.Message, args: Li
     session.commit()
 
     await message.channel.send(
-        f"👢 Le membre **{message.mentions[0]}** a été kické (id `k{new_kick.id}`):\n{current_reason}")
+        embed=EmbedsManager.complete_embed(
+            f"👢 Le membre **{message.mentions[0]}** a été kické (id `k{new_kick.id}`):\n{current_reason}"
+        )
+    )
 
     await message.mentions[0].kick()
