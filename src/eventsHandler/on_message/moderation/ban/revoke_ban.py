45e3386d51db7ada17325d25815e26286386b221
Quentin Briolant <quentin.briolant@epita.fr>
2020-02-18 22:06:58 +0000

    fix (revoke_ban): update code

--- src/eventsHandler/on_message/moderation/ban/revoke_ban.py
+++ src/eventsHandler/on_message/moderation/ban/revoke_ban.py
@@ -1,33 +1,71 @@
 from typing import List
 import discord
+import yaml
+
 from src.models.models import Ban, session
+from src.utils.embeds_manager import EmbedsManager
+from src.utils.permissions_manager import PermissionsManager
 
 
 async def revoke_ban(client: discord.Client, message: discord.Message, args: List[str]):
-    if not message.author.guild_permissions.ban_members:
-        await message.channel.send(f":x: Vous n'avez pas les permissions pour cette commande.")
-        return
+    with open('run/config/config.yml', 'r') as file:
+        config = yaml.safe_load(file)
 
+    # Check permissions
+    if not PermissionsManager.has_perm(message.author, 'ban'):
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                "Vous n'avez pas les permissions pour cette commande."
+            )
+        )
+
+    # Help message
+    if args and args[0] == '-h':
+        return await message.channel.send(
+            embed=EmbedsManager.information_embed(
+                "Rappel de la commande : \n"
+                f"`{config['prefix']}rban <ban_id>`"
+            )
+        )
+
+    # Check inputs
     if len(args) != 1:
-        await message.channel.send(f":x: Erreur dans la commande, merci de spécifier l'index du ban.")
-        return
+        return message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur dans la commande, merci de spécifier l'index du ban."
+            )
+        )
 
     if not args[0].startswith("b"):
-        await message.channel.send(f":x: Erreur, index invalide.")
-        return
+        return message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, index invalide."
+            )
+        )
 
+    # Process code
     index = int(args[0][1:])
     current_ban: Ban = session.query(Ban).filter_by(id=index).first()
 
     if current_ban is None:
-        await message.channel.send(f":x: Erreur, index invalide.")
-        return
+        return message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, index invalide."
+            )
+        )
 
     if not current_ban.is_active:
-        await message.channel.send(f":x: Erreur, ce bannissement est déjà révoqué.")
-        return
+        return message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, ce bannissement est déjà révoqué."
+            )
+        )
 
     current_ban.is_active = False
     session.commit()
 
-    await message.channel.send(f"🔨 Le bannissement **{args[0]}** a été révoqué.")
+    await message.channel.send(
+        embed=EmbedsManager.complete_embed(
+            "🔨 Le bannissement **{args[0]}** a été révoqué."
+        )
+    )
