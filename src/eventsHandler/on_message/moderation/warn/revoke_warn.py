93d6fd01e5f409f99cfd77ee8a957e867dc20fe6
Quentin Briolant <quentin.briolant@epita.fr>
2020-02-19 23:13:32 +0000

    fix (rwarn): update code

--- src/eventsHandler/on_message/moderation/warn/revoke_warn.py
+++ src/eventsHandler/on_message/moderation/warn/revoke_warn.py
@@ -1,33 +1,71 @@
 from typing import List
 import discord
+import yaml
+
 from src.models.models import Ban, session, Warn
+from src.utils.embeds_manager import EmbedsManager
+from src.utils.permissions_manager import PermissionsManager
 
 
 async def revoke_warn(client: discord.Client, message: discord.Message, args: List[str]):
-    if not message.author.guild_permissions.manage_messages:
-        await message.channel.send(f":x: Vous n'avez pas les permissions pour cette commande.")
-        return
+    with open('run/config/config.yml', 'r') as file:
+        config = yaml.safe_load(file)
 
+    # Check permissions
+    if not PermissionsManager.has_perm(message.author, 'manage_reason'):
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                "Vous n'avez pas les permissions pour cette commande."
+            )
+        )
+
+    # Help message
+    if args and args[0] == '-h':
+        return await message.channel.send(
+            embed=EmbedsManager.information_embed(
+                "Rappel de la commande : \n"
+                f"`{config['prefix']}rwarn <warn_id>`"
+            )
+        )
+
+    # Check inputs
     if len(args) != 1:
-        await message.channel.send(f":x: Erreur dans la commande, merci de spécifier l'index de l'avertissement.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                f":x: Erreur dans la commande, merci de spécifier l'index de l'avertissement."
+            )
+        )
 
     if not args[0].startswith("w"):
-        await message.channel.send(f":x: Erreur, index invalide.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                f":x: Erreur, index invalide."
+            )
+        )
 
+    # Process code
     index = int(args[0][1:])
     current_warn: Warn = session.query(Warn).filter_by(id=index).first()
 
     if current_warn is None:
-        await message.channel.send(f":x: Erreur, index invalide.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                f":x: Erreur, index invalide."
+            )
+        )
 
     if not current_warn.is_active:
-        await message.channel.send(f":x: Erreur, cet avertissement est déjà révoqué.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, cet avertissement est déjà révoqué."
+            )
+        )
 
     current_warn.is_active = False
     session.commit()
 
-    await message.channel.send(f"🔨 L'avertissement **{args[0]}** a été révoqué.")
+    await message.channel.send(
+        embed=EmbedsManager.complete_embed(
+            f"🔨 L'avertissement **{args[0]}** a été révoqué."
+        )
+    )
