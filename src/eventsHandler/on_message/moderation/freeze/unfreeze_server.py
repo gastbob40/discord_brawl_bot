9775b005f8f87dc2c2f7646362c37cf43e695b09
Quentin Briolant <quentin.briolant@epita.fr>
2020-02-19 23:49:01 +0000

    fix (unfreeze): update code

--- src/eventsHandler/on_message/moderation/freeze/unfreeze_server.py
+++ src/eventsHandler/on_message/moderation/freeze/unfreeze_server.py
@@ -2,34 +2,68 @@ import asyncio
 from typing import List
 
 import discord
+import yaml
+
+from src.utils.embeds_manager import EmbedsManager
+from src.utils.permissions_manager import PermissionsManager
 
 
 async def unfreeze_server(client: discord.Client, message: discord.Message, args: List[str]):
-    if not message.author.guild_permissions.administrator:
-        await message.channel.send(f":x: Vous n'avez pas les permissions pour cette commande.")
-        return
+    with open('run/config/config.yml', 'r') as file:
+        config = yaml.safe_load(file)
 
-    current_message: discord.Message = await message.channel.send \
-        (f"{message.author.mention} Vous avez décidé de dégeler le serveur.\n" +
-         f"Confirmer vous ce choix ?")
+    # Check permissions
+    if not PermissionsManager.has_perm(message.author, 'freeze'):
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                "Vous n'avez pas les permissions pour cette commande."
+            )
+        )
+
+    # Help message
+    if args and args[0] == '-h':
+        return await message.channel.send(
+            embed=EmbedsManager.information_embed(
+                "Rappel de la commande : \n"
+                f"`{config['prefix']}unfreeze`"
+            )
+        )
+
+    # Check inputs
+    current_message: discord.Message = await message.channel.send(
+        embed=EmbedsManager.complete_embed(
+            f"{message.author.mention} Vous avez décidé de dégeler le serveur.\n" +
+            f"Confirmer vous ce choix ?"
+        )
+    )
 
     await current_message.add_reaction('✅')
     await current_message.add_reaction('❌')
 
-    def check(reaction, user):
+    def check(reaction: discord.Reaction, user: discord.User):
         return user == message.author and (str(reaction.emoji) == '✅' or str(reaction.emoji) == '❌')
 
     try:
         reaction, user = await client.wait_for('reaction_add', timeout=20.0, check=check)
     except asyncio.TimeoutError:
-        await message.channel.send(f"Vous avez __refusé__ le dégel du serveur.")
+        await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                f"Vous avez __refusé__ le dégel du serveur."
+            )
+        )
     else:
         if str(reaction.emoji) == '❌':
-            await message.channel.send(
-                f"Vous avez __refusé__ le dégel du serveur.")
-            return
+            return await message.channel.send(
+                embed=EmbedsManager.error_embed(
+                    f"Vous avez __refusé__ le dégel du serveur."
+                )
+            )
 
-        await message.channel.send(f"Le serveur a été dégelé")
+        await message.channel.send(
+            embed=EmbedsManager.complete_embed(
+                f"Le serveur a été dégelé"
+            )
+        )
 
         members: List[discord.Member] = message.guild.members
 
@@ -39,4 +73,3 @@ async def unfreeze_server(client: discord.Client, message: discord.Message, args
                     if not member.permissions_in(channel).send_messages:
                         await channel.set_permissions(member,
                                                       overwrite=None)
-
