eec745accd53b374f58c9b09e43c9c73bda25167
Quentin Briolant <quentin.briolant@epita.fr>
2020-02-19 23:30:29 +0000

    fix (rmute): update code

--- src/eventsHandler/on_message/moderation/mute/revoke_mute.py
+++ src/eventsHandler/on_message/moderation/mute/revoke_mute.py
@@ -1,31 +1,62 @@
 from typing import List
 import discord
+import yaml
+
 from src.models.models import Mute, session
+from src.utils.embeds_manager import EmbedsManager
+from src.utils.permissions_manager import PermissionsManager
 
 
 async def revoke_mute(client: discord.Client, message: discord.Message, args: List[str]):
-    if not message.author.guild_permissions.manage_messages:
-        await message.channel.send(f":x: Vous n'avez pas les permissions pour cette commande.")
-        return
+    with open('run/config/config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    if not PermissionsManager.has_perm(message.author, 'manage_reason'):
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                "Vous n'avez pas les permissions pour cette commande."
+            )
+        )
+
+    # Help message
+    if args and args[0] == '-h':
+        return await message.channel.send(
+            embed=EmbedsManager.information_embed(
+                "Rappel de la commande : \n"
+                f"`{config['prefix']}rmute <mute_id>`"
+            )
+        )
 
     if len(args) != 1:
-        await message.channel.send(f":x: Erreur dans la commande, merci de spécifier l'index du mute.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                f":x: Erreur dans la commande, merci de spécifier l'index du mute."
+            )
+        )
 
     if not args[0].startswith("m"):
-        await message.channel.send(f":x: Erreur, index invalide.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, index invalide."
+            )
+        )
 
     index = int(args[0][1:])
     current_mute: Mute = session.query(Mute).filter_by(id=index).first()
 
     if current_mute is None:
-        await message.channel.send(f":x: Erreur, index invalide.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, index invalide."
+            )
+        )
 
     if not current_mute.is_active:
-        await message.channel.send(f":x: Erreur, ce mute est déjà révoqué.")
-        return
+        return await message.channel.send(
+            embed=EmbedsManager.error_embed(
+                ":x: Erreur, ce mute est déjà révoqué."
+            )
+        )
 
     current_mute.is_active = False
     session.commit()
@@ -36,4 +67,8 @@ async def revoke_mute(client: discord.Client, message: discord.Message, args: Li
             await channel.set_permissions(target,
                                           overwrite=None)
 
-    await message.channel.send(f"⚠ Le mute **{args[0]}** a été révoqué.")
+    await message.channel.send(
+        embed=EmbedsManager.complete_embed(
+            f"⚠ Le mute **{args[0]}** a été révoqué."
+        )
+    )
